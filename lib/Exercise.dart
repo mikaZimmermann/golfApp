7fea4ac49e05a2bf16d4cdbb3af6cfcefffd1255
mikaZimmermann <40148700+mikaZimmermann@users.noreply.github.com>
2023-06-02 00:12:41 +0200

    Added more classes

---
+++ lib/Exercise.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:version1/Shottype.dart';
+
+import 'ExecutionRecord.dart';
+
+class Exercise extends StatefulWidget {
+  const Exercise({super.key, required this.id, required this.focusPoints, required this.records, required this.shottypes});
+  final int id;
+  final List<String> focusPoints;
+  final List<ExecutionRecord> records;
+  final List<Shottype> shottypes;
+  @override
+  State<Exercise> createState() => _ExerciseState();
+}
+
+
+
+class _ExerciseState extends State<Exercise> {
+ /*
+ ____________________________________________________________________________________
+ This class needs to destroy its parent object after creating a
+ new instance with the lastest execution added to List<ExecutionRecord>
+ records of parent records
+ 
+  */ 
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/ExerciseDescription.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClassName {
+
+  ClassName(this.name, this.Illustration, this.content, this.repetitions, this.passingThreshold);
+  
+
+  final String name;
+  final Image Illustration;
+  final List<String> content;
+  final int repetitions;
+  final String passingThreshold;
+
+  
+}
